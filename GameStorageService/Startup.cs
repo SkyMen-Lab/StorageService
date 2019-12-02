a63fc1cf5adb518b916ff97f3c9983ba2c6cab28
German <german.nikolishin@gmail.com>
2019-12-02 09:54:37 +0000

    project files

--- GameStorageService/Startup.cs
+++ GameStorageService/Startup.cs
@@ -42,10 +42,7 @@ namespace GameStorageService
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
