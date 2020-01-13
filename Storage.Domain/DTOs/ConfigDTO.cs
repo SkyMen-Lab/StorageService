b81a7afd039f04fff853fd5791447c8bbb0ac42f
German <german.nikolishin@gmail.com>
2020-01-13 10:15:45 +0000

    IP validation issue #5

--- Storage.Domain/DTOs/ConfigDTO.cs
+++ Storage.Domain/DTOs/ConfigDTO.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Storage.Domain.Customs;
 using Storage.Domain.Models;
 
 namespace Storage.Domain.DTOs
@@ -8,6 +9,7 @@ namespace Storage.Domain.DTOs
     {
         [MinLength(7, ErrorMessage = "Invalid IP address")]
         [Required]
+        [IPValidate]
         public string RouterIpAddress { get; set; }
         [Required]
         [Range(1025, 65535, ErrorMessage = "Port is out of the range (1025, 65535")]
--- Storage.Domain/Models/Config.cs
+++ Storage.Domain/Models/Config.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Storage.Domain.Customs;
 
 namespace Storage.Domain.Models
 {
@@ -9,6 +10,7 @@ namespace Storage.Domain.Models
         public int Id { get; set; }
         [MinLength(7, ErrorMessage = "Invalid IP address")]
         [Required]
+        [IPValidate]
         public string RouterIpAddress { get; set; }
         [Required]
         [Range(1025, 65535, ErrorMessage = "Port is out of the range (1025, 65535")]
